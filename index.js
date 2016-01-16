1f877cd1e1728709c1af21bee3377f548ad8bcc6
Karissa McKelvey <krmckelv@gmail.com>
2016-01-15 17:10:10 -0800

    make it work

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
-var level = require('level-browserify')
+var memdb = require('memdb')
 var dat = require('dat')
-var swarm = require('./swarm.js')
 var browserfs = require('./fs.js')
 
 module.exports = function (opts) {
   if (!opts) opts = {}
-  if (!opts.signalhub) throw new Error('Requires signalhub.')
-  if (!opts.db) opts.db = level('./datdb')
+  if (!opts.db) opts.db = memdb('dat-data')
   if (!opts.fs) opts.fs = browserfs
-  opts.discovery = swarm(opts.signalhub)
+  opts.discovery = false
   return dat(opts)
 }
