7f540f4a2c32addd1ad0d123ca316132a3b40324
Karissa McKelvey <krmckelv@gmail.com>
2016-01-18 17:55:29 -0800

    use level-browserify for default persistance

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-var memdb = require('memdb')
+var level = require('level-browserify')
 var dat = require('dat')
 var browserfs = require('./fs.js')
 
 module.exports = function (opts) {
   if (!opts) opts = {}
-  if (!opts.db) opts.db = memdb('dat-data')
+  if (!opts.db) opts.db = level('./datdata')
   if (!opts.fs) opts.fs = browserfs
   opts.discovery = false
   return dat(opts)
