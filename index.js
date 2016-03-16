772f47dc47adbe4e6b4bd696506a63c1618f34d7
Karissa McKelvey <krmckelv@gmail.com>
2016-03-16 15:26:46 -0700

    update for new dat using hyperdrive and webrtc swarm directly

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
 var level = require('level-browserify')
-var dat = require('dat')
-var browserfs = require('./fs.js')
+var pump = require('pump')
+var debug = require('debug')('dat-browserify')
+var hyperdrive = require('hyperdrive')
+var webrtcSwarm = require('webrtc-swarm')
+var signalhub = require('signalhub')
 
-module.exports = function (opts) {
+var DEFAULT_SIGNALHUB = 'https://signalhub.publicbits.org'
+
+module.exports = Dat
+
+function Dat (opts) {
+  if (!(this instanceof Dat)) return new Dat(opts)
   if (!opts) opts = {}
-  if (!opts.db) opts.db = level('./datdata')
-  if (!opts.fs) opts.fs = browserfs
-  opts.discovery = false
-  return dat(opts)
+  if (!opts.db) opts.db = level('./dat')
+  this.drive = hyperdrive(opts.db)
+}
+
+Dat.prototype.join = function (link, opts) {
+  var self = this
+  if (!opts) opts = {}
+  opts.signalhub = opts.signalhub || DEFAULT_SIGNALHUB
+  link = link.replace('dat://', '').replace('dat:', '')
+  var key = link.toString('hex')
+  var hub = signalhub('hyperdrive/' + key, [opts.signalhub])
+  var swarm = webrtcSwarm(hub, opts)
+
+  swarm.on('peer', function (peer) {
+    console.log('got a peer', 'peer')
+    pump(peer, self.drive.createPeerStream(), peer, function (err) {
+      if (err) throw err
+    })
+  })
+
+  return swarm
 }
